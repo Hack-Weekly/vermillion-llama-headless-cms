2db53813fdcf6954f0328739e86ec59860ba60b9
tcq1 <chu.trung@web.de>
2023-06-10 16:18:00 +0200

    Remove unnecessary annotations

--- src/main/java/com/llama/headlessCMS/repository/ContentRepository.java
+++ src/main/java/com/llama/headlessCMS/repository/ContentRepository.java
@@ -2,15 +2,11 @@ package com.llama.headlessCMS.repository;
 
 import com.llama.headlessCMS.model.Content;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface ContentRepository extends MongoRepository<Content, String> {
-
-    @Query("{author:  ?0} ")
     List<Content> findByAuthor(String author);
 
-    @Query("{title:  ?0} ")
     List<Content> findByTitle(String title);
 }
